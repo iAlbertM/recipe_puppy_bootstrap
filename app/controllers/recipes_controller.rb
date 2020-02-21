3517786d143ffd7659f71ffed0577cccb4e53558
Albert Martinez <albertwdd@gmail.com>
2020-02-21 12:29:01 -0800

    update app

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,14 @@
+require "awesome_print"
+  
+class RecipesController < ApplicationController
+  def index
+  	# Inside the index action, you will need to 
+  		# create 2 instance variables: 
+  		# a. @search- this is the value of the query parameter search (see the screenshot above) if one is provided or chocolate if no search query parameter is present (default). 
+
+  		# b. @recipes - this is an array of recipes that you will get by calling the class method for of the Recipe class (model) available to you under app/models/recipe.rb with the @search variable you created right above.
+    @search = params[:query] || "chocolate"
+    # :query["keyword"] = :q
+    @recipes = Recipe.for(@search)
+  end
+end
