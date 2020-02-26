62169e6934ea836d3973f103be37e4450d8346c1
Albert Martinez <albertwdd@gmail.com>
2020-02-25 17:44:05 -0800

    render results in json

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,7 +6,7 @@ class Recipe
     base_uri 'http://www.recipepuppy.com/api'
     default_params onlyImages: 1
     # ENABLE THE FOLLOWING LINE ONCE recipepuppy.com fully working...
-    # format :json
+    # format :jsond
 
      def self.for ( keyword = 'chocolate' )
         get('', query: { q: keyword })['results']
